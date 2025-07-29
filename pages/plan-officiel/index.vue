718305b57d3065e28fa3a310245c5e055a714913
velooise <webmaster@velooise.fr>
2025-07-29 08:33:53 +0200

    Update index.vue

--- pages/plan-officiel/index.vue
+++ pages/plan-officiel/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full">
     <img
-      src="https://cyclopolis.lavilleavelo.org/carte-voies-lyonnaises-2026.png"
+      src="https://www.velooise.fr/wp-content/uploads/2025/07/20212007.CCAC_SDC_COPIL_20211007.jpg"
       class="object-contain w-full h-screen"
       :alt="`plan officiel des ${getRevName()}`"
     >
@@ -11,16 +11,15 @@
 <script setup lang="ts">
 const { getRevName } = useConfig();
 
-const description = `Découvrez le plan officiel des ${getRevName()}, le futur réseau vélo lyonnais de 260km.`;
+const description = `Découvrez le plan officiel du schéma Cyclable de l'Aire Cantilienne`;
 const COVER_IMAGE_URL = 'https://cyclopolis.lavilleavelo.org/cyclopolis.png';
 
 useHead({
-  title: `Plan des ${getRevName()}`,
+  title: `Schéma cyclable de l'Aire Cantilienne`,
   meta: [
     // description
     { hid: 'description', name: 'description', content: description },
     { hid: 'og:description', property: 'og:description', content: description },
-    { hid: 'twitter:description', name: 'twitter:description', content: description },
     // cover image
     { hid: 'og:image', property: 'og:image', content: COVER_IMAGE_URL },
     { hid: 'twitter:image', name: 'twitter:image', content: COVER_IMAGE_URL }
