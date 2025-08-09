85122b35864a2ffe0cbc41ef6a4f88f32ab04f41
velooise <webmaster@velooise.fr>
2025-08-09 09:33:32 +0200

    Update FilterModal.vue add imp+debouche-cyclable

--- components/FilterModal.vue
+++ components/FilterModal.vue
@@ -89,6 +89,7 @@ const typeFilters = ref([
   { label: 'Bandes cyclables', isEnable: true, types: ['bandes-cyclables'] },
   { label: 'Zone de rencontre', isEnable: true, types: ['zone-de-rencontre'] },
   { label: 'Zone 30', isEnable: true, types: ['zone-30'] },
+  { label: 'Impasse + débouché cyclable', isEnable: true, types: ['imp+debouche-cyclable'] },
   { label: 'Inconnu', isEnable: true, types: ['inconnu'] },
   { label: 'Aucun', isEnable: true, types: ['aucun'] }
 ]);
