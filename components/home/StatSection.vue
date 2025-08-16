dfb9b79d20b110c30c7e2985614463dde29c5330
thierrich <thierry.roch@gmail.com>
2025-08-16 20:44:40 +0200

    Update StatSection.vue

--- components/home/StatSection.vue
+++ components/home/StatSection.vue
@@ -10,25 +10,25 @@
         <p class="mt-3 text-xl text-gray-500 sm:mt-4">
           Plusieurs communes et communautés de communes du Sud de l'Oise ont réalisé un schéma cyclable, VélOOise a participé à la plupart de ces concertations. Un schéma c'est une projection pour les dix années à venir environ qui identifie les liaisons cyclables souhaitées, leur type et une estimation du coût.  L'ensemble doit créer un maillage cyclable sans discontinuités.
         </p>
+       <P>
         Les schémas cyclables qui sont incorporés dans CycloPolis de VélOOise :
+         </P>
         <ul>
-          <li> Schéma de la Communauté de Communes de l'Aire Cantilienne (99%) </li>
-          <li> Schéma de l'ARC & Plan Vélo (Agglomération de Compiègne)  (1% en cours)</li>
-          <li> Schéma de l'ACSO (10%)</li>
-          <li> Schéma du Clermontois (10%)</li>
-          <li> Schéma de la ville de Senlis (100%)  </li>
-          <li> Schéma de la ville de Chantilly (0%)  </li>
+          <li> - Schéma de la Communauté de Communes de l'Aire Cantilienne (99%) </li>
+          <li> - Schéma de l'ARC & Plan Vélo (Agglomération de Compiègne)  (1% en cours)</li>
+          <li> - Schéma de l'ACSO (10%)</li>
+          <li> - Schéma du Clermontois (10%)</li>
+          <li> - Schéma de la ville de Senlis (100%)  </li>
+          <li> - Schéma de la ville de Chantilly (0%)  </li>
             
         </ul>
+      
         D'autres schémas viendront s'adjoindre à cet outil. L'objectif de l'outil est de suivre le déploiement des différents plans et de constituer un historique des différents aménagements. C'est un travail un peu fastidieux à réaliser et <b>tous les volontaires sont invités à participer</b>. Il suffit de manipuler un peu en informatique, nous pouvons vous former, si vous connaissez github, les formats json et markdown, c'est un plus.
       </div>
     </div>
    
 </template>
 <script setup lang="ts">
-
-
 const { getRevName, getNbVoiesCyclables } = useConfig();
-
 
 </script>
