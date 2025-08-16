5d63a6adc98cde02373e6f88055209caafaea807
thierrich <thierry.roch@gmail.com>
2025-08-16 19:59:24 +0200

    Update StatSection.vue

--- components/home/StatSection.vue
+++ components/home/StatSection.vue
@@ -6,46 +6,22 @@
           Les {{ getRevName() }}, c'est quoi ?
         </h2>
         <p class="mt-3 text-xl text-gray-500 sm:mt-4">
-          C'est le nom donné au projet de réseau cyclable porté par la Métropole de Lyon.<br>Concrètement, une {{ getRevName('singular') }} est une voie cyclable large (3 à 4m), souvent sous forme de piste bidirectionnelle et séparée des voitures. L'ensemble doit créer un maillage cyclable sans discontinuités qui connecte les quartiers lyonnais aux communes de la périphérie de la métropole.
+          Plusieurs communes et communautés de communes du Sud de l'Oise ont réalisé un schéma cyclable, VélOOise a participé à la plupart de ces concertations. Un schéma c'est une projection pour les dix années à venir environ qui identifie les liaisons cyclables souhaitées, leur type et une estimation du coût.  L'ensemble doit créer un maillage cyclable sans discontinuités.
         </p>
+        Les schémas cyclables qui sont incorporés dans CycloPolis de VélOOise :
+        <ul>
+          <li> Schéma de la Communauté de Communes de l'Aire Cantilienne (99%)
+          <li> Schéma de l'ARC & Plan Vélo (Agglomération de Compiègne)  (1% en cours)
+          <li> Schéma de l'ACSO (10%)
+          <li> Schéma du Clermontois (10%)
+          <li> Schéma de la ville de Senlis (100%)  
+          <li> Schéma de la ville de Chantilly (0%)  
+            
+        </ul>
+        D'autres schémas viendront s'adjoindre à cet outil. L'objectif de l'outil est de suivre le déploiement des différents plans et de constituer un historique des différents aménagements. C'est un travail un peu fastidieux à réaliser et tous les volontaires sont invités à participer. Il suffit de manipuler un peu en informatique, nous pouvons vous former, si vous connaissez github, les formats json et markdown, c'est un plus.
       </div>
     </div>
-    <div class="mt-10 bg-white">
-      <div class="relative">
-        <div class="absolute inset-0 h-1/2 bg-white" />
-        <div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div class="max-w-4xl mx-auto">
-            <dl class="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-3">
-              <div class="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
-                <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-                  Lignes
-                </dt>
-                <dd class="order-1 text-5xl font-extrabold text-lvv-blue-600">
-                  {{ getNbVoiesCyclables() }}
-                </dd>
-              </div>
-              <div class="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
-                <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-                  Distance
-                </dt>
-                <dd class="order-1 text-5xl font-extrabold text-lvv-blue-600">
-                  260km
-                </dd>
-              </div>
-              <div class="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
-                <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-                  Budget
-                </dt>
-                <dd class="order-1 text-5xl font-extrabold text-lvv-blue-600">
-                  280M€
-                </dd>
-              </div>
-            </dl>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+    
 </template>
 
 <script setup lang="ts">
