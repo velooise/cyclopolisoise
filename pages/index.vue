1f9160e1e622ee68b0733ec1c1967f3b62aeaf4f
thierrich <thierry.roch@gmail.com>
2025-08-18 08:15:14 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -25,7 +25,7 @@
             Avancement par ligne
           </h2>
           <p class="text-xl text-gray-500">
-            Choisissez une {{ getRevName('singular') }} pour connaitre le détail du projet et voir son niveau d'avancement.
+            Choisissez une liaison pour connaitre le détail du projet et voir son niveau d'avancement.
           </p>
         </div>
         <HomeLinesSection class="mt-5" />
