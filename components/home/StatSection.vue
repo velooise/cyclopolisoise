1d97aff1ed878144ac609b48a6e7827600542455
thierrich <thierry.roch@gmail.com>
2025-08-19 10:58:57 +0200

    Update StatSection.vue

--- components/home/StatSection.vue
+++ components/home/StatSection.vue
@@ -16,7 +16,7 @@
         <ul>
           <li> - Schéma de la Communauté de Communes de l'Aire Cantilienne (99%) lignes 1 à 39 - 2021 - <a href=https://nuage.velooise.fr/index.php/s/iTc7NrdKcF6p47G>Programme</a> </li>
           <li> - Schéma de la CCSSO (Communauté de Communes Senlis Sud Oise)  (0% en cours) lignes 200-299 - 2020 <a href=https://nuage.velooise.fr/index.php/s/7J8ECTzccJ5dgmL>Programme</a> </li>
-          <li> - Schéma de l'ARC & Plan Vélo (Agglomération de Compiègne)  (1% en cours) lignes 70-99 <a href=https://geo.compiegnois.fr/adws/app/9f32f5a7-1f4a-11eb-9fe5-85eb52a29e6a/index.html>Plan Vélo 2021-2026</a> </li>
+          <li> - Schéma de l'ARC & Plan Vélo (Agglomération de Compiègne)  (1% en cours) lignes 100-199 <a href=https://geo.compiegnois.fr/adws/app/9f32f5a7-1f4a-11eb-9fe5-85eb52a29e6a/index.html>Plan Vélo 2021-2026</a> </li>
           <li> - Schéma de l'ACSO (10%) lignes 100-119 - 2019 - <a href=https://nuage.velooise.fr/index.php/s/XbaPero22LJQHGz>Programme</a></li>
           <li> - Schéma du Clermontois (10%) lignes 120-139 - 2022 - <a href=https://nuage.velooise.fr/index.php/s/5psYyQNQz9T9jf2>Programme</a></li>
           <li> - Schéma de la ville de Senlis (100%) lignes 40 à 59 - Septembre 2022 - <a href=https://nuage.velooise.fr/index.php/s/kfCECc6bbQyBZPq>Programme</a> </li>
