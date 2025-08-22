85b57bee1bdcb7f930cea8531627312578fd5d93
thierrich <thierry.roch@gmail.com>
2025-08-22 16:29:40 +0200

    Update Map.vue

--- components/Map.vue
+++ components/Map.vue
@@ -59,7 +59,7 @@ const {
 } = useMap();
 
 const statuses = ref(['planned', 'variante', 'done', 'postponed', 'variante-postponed', 'unknown', 'wip', 'tested']);
-const types = ref(['bidirectionnelle', 'bilaterale', 'voie-bus', 'voie-bus-elargie', 'velorue', 'voie-verte', 'bandes-cyclables', 'zone-de-rencontre','zone-30', 'imp+debouche-cyclable', 'piste-sur-trottoir','chaucidou', 'jalonnement’, 'pictogramme', 'voie-riverains', 'unidirectionnelle','aucun', 'inconnu']);
+const types = ref(['bidirectionnelle', 'bilaterale', 'voie-bus', 'voie-bus-elargie', 'velorue', 'voie-verte', 'bandes-cyclables', 'zone-de-rencontre','zone-30', 'imp+debouche-cyclable', 'piste-sur-trottoir','chaucidou', 'jalonnement', 'pictogramme', 'voie-riverains', 'unidirectionnelle','aucun', 'inconnu']);
 
 const features = computed(() => {
   return (props.features ?? []).filter(feature => {
