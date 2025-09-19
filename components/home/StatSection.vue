76f71c899e127f2ef5b57b6c82ec4953bdd33233
thierrich <thierry.roch@gmail.com>
2025-09-19 23:00:32 +0200

    Update StatSection.vue

--- components/home/StatSection.vue
+++ components/home/StatSection.vue
@@ -15,14 +15,15 @@
          </P>
         <ul>
           <li> - Schéma de la Communauté de Communes de l'Aire Cantilienne (intègré TR) lignes 1 à 39 - 2021 - <a href=https://nuage.velooise.fr/index.php/s/iTc7NrdKcF6p47G>Programme</a> </li>
-          <li> - Schéma de la CCSSO (Communauté de Communes Senlis Sud Oise)  (intégré NH+TR) lignes 200-299 - 2020 <a href=https://nuage.velooise.fr/index.php/s/7J8ECTzccJ5dgmL>Programme</a> </li>
+       <li> - Schéma de la ville de Senlis (intègré TR) lignes 40 à 59 - Septembre 2022 - <a href=https://nuage.velooise.fr/index.php/s/kfCECc6bbQyBZPq>Programme</a> </li>
+        
           <li> - Schéma de l'ARC & Plan Vélo (Agglomération de Compiègne)  (intègré TR) lignes 100-199 <a href=https://geo.compiegnois.fr/adws/app/9f32f5a7-1f4a-11eb-9fe5-85eb52a29e6a/index.html>Plan Vélo 2021-2026</a> </li>
-          <li> - Schéma de l'ACSO (10%) lignes 300-399 - 2019 - <a href=https://nuage.velooise.fr/index.php/s/XbaPero22LJQHGz>Programme</a></li>
-          <li> - Schéma de la Communauté de Communes du Liancourtois et de la Vallée Dorée (100%) lignes 600-799 - 2022 - <a href=https://nuage.au5v.fr/index.php/s/4yjttrmBdQsZdZX>Programme</a></li>
+          <li> - Schéma de la CCSSO (Communauté de Communes Senlis Sud Oise)  (intégré NH+TR) lignes 200-299 - 2020 <a href=https://nuage.velooise.fr/index.php/s/7J8ECTzccJ5dgmL>Programme</a> </li>
+          <li> - Schéma de la ville de Chantilly (0%) lignes 300 - 399 - Mars 2022 - <a href=https://nuage.velooise.fr/index.php/s/487T6HwCcsYtoQF>Programme</a> </li>
           <li> - Schéma du Clermontois (10%) lignes 400-499 - 2022 - <a href=https://nuage.velooise.fr/index.php/s/5psYyQNQz9T9jf2>Programme</a></li>
-         <li> - Schéma de la CCPOH (Communauté de Communes du Pays d'Oise et d'Halatte) - 2021 (intègré TR) lignes 500-599 <a href=https://nuage.au5v.fr/index.php/s/Tr7gjdXyYPnPa8p>Scénario Retenu</a> </li>
-          <li> - Schéma de la ville de Senlis (intègré TR) lignes 40 à 59 - Septembre 2022 - <a href=https://nuage.velooise.fr/index.php/s/kfCECc6bbQyBZPq>Programme</a> </li>
-          <li> - Schéma de la ville de Chantilly (0%) lignes 500 - 599 - Mars 2022 - <a href=https://nuage.velooise.fr/index.php/s/487T6HwCcsYtoQF>Programme</a> </li>
+          <li> - Schéma de la CCPOH (Communauté de Communes du Pays d'Oise et d'Halatte) - 2021 (intègré TR) lignes 500-599 <a href=https://nuage.au5v.fr/index.php/s/Tr7gjdXyYPnPa8p>Scénario Retenu</a> </li>
+          <li> - Schéma de la Communauté de Communes du Liancourtois et de la Vallée Dorée (100%) lignes 600-799 - 2022 - <a href=https://nuage.au5v.fr/index.php/s/4yjttrmBdQsZdZX>Programme</a></li>
+           <li> - Schéma de l'ACSO (10%) lignes 800-999 - 2019 - 2025 <a href=https://nuage.velooise.fr/index.php/s/XbaPero22LJQHGz>Programme</a></li>
             
         </ul>
       
