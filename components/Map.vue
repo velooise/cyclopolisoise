2cc5b9df05947de45f4516b7ebc52f08779d2b97
Guillaume Lostis <guillaume@lostis.com>
2025-11-01 22:03:56 +0100

    Logo Lyon → VéloOise

--- components/Map.vue
+++ components/Map.vue
@@ -6,7 +6,7 @@
     <img
       v-if="options.logo"
       class="my-0 absolute bottom-0 right-0 z-10"
-      src="https://cyclopolis.lavilleavelo.org/logo-lvv-carte.png"
+      src="https://www.velooise.fr/wp-content/uploads/2025/08/cyclopolis-header-velooise.png"
       width="75"
       height="75"
       :alt="`logo ${config.assoName}`"
