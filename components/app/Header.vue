778eccb93a4e6eda8c99e714519f0b77c4bdb940
velooise <webmaster@velooise.fr>
2026-08-06 01:35:55 +0200

    retrait compteurs

--- components/app/Header.vue
+++ components/app/Header.vue
@@ -139,60 +139,7 @@
           </Popover>
 
           <!-- Compteurs -->
-          <Popover v-slot="{ open }" class="relative">
-            <PopoverButton
-              :class="[
-                open ? 'text-gray-900' : 'text-gray-500',
-                'group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-lvv-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-lvv-blue-600 focus-visible:ring-offset-2',
-              ]"
-            >
-              <span>Compteurs</span>
-              <Icon
-                name="mdi:chevron-down"
-                :class="[open ? 'text-gray-600' : 'text-gray-400', 'ml-2 h-5 w-5 group-hover:text-gray-500']"
-                aria-hidden="true"
-              />
-            </PopoverButton>
-            <transition
-              enter-active-class="transition ease-out duration-200"
-              enter-from-class="opacity-0 translate-y-1"
-              enter-to-class="opacity-100 translate-y-0"
-              leave-active-class="transition ease-in duration-150"
-              leave-from-class="opacity-100 translate-y-0"
-              leave-to-class="opacity-0 translate-y-1"
-            >
-              <PopoverPanel
-                v-slot="{ close }"
-                class="absolute left-1/2 z-10 mt-3 w-screen md:w-max max-w-md -translate-x-1/2 transform px-2 sm:px-0"
-              >
-                <div class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white">
-                  <div class="p-4 flex flex-col gap-2">
-                    <NuxtLink
-                      to="/compteurs/velo"
-                      class="text-base font-medium text-gray-500 hover:text-lvv-blue-600"
-                      @click="close()"
-                    >
-                      Compteurs vélo
-                    </NuxtLink>
-                    <NuxtLink
-                      to="/compteurs/voiture"
-                      class="text-base font-medium text-gray-500 hover:text-lvv-blue-600"
-                      @click="close()"
-                    >
-                      Compteurs voiture
-                    </NuxtLink>
-                    <NuxtLink
-                      to="/compteurs/comparaison"
-                      class="text-base font-medium text-gray-500 hover:text-lvv-blue-600"
-                      @click="close()"
-                    >
-                      Comparaison voiture/vélo
-                    </NuxtLink>
-                  </div>
-                </div>
-              </PopoverPanel>
-            </transition>
-          </Popover>
+  
         </PopoverGroup>
         <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0 gap-1">
           <div class="flex items-center -space-x-0.5">
